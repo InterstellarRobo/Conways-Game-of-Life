a5b07a535feb88abc9f5595e6f2447eab27c15be
Fred Cadet <46660453+InterstellarRobo@users.noreply.github.com>
2023-05-26 19:13:54 -0500

    GUI? Nah

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Game of life :)");
+        JFrame f = new JFrame();
+        JLabel o = new JLabel("Conway's Game of Life :)");
+        f.add(o);
+        f.setVisible(true);
     }
 }
