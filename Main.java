b6f02c21c01f6951a979817c07ddce36f6b74aec
Fred Cadet <46660453+InterstellarRobo@users.noreply.github.com>
2023-06-03 15:07:37 -0500

    Just testing stuff

--- Main.java
+++ Main.java
@@ -1,10 +1,16 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         JFrame f = new JFrame();
         JLabel o = new JLabel("Conway's Game of Life :)");
+        JButton b = new JButton("Just testing stuff");
+        b.setFocusable(false);
+
+        f.setLayout(new FlowLayout());
         f.add(o);
+        f.add(b);
         f.pack();
         f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         f.setVisible(true);
